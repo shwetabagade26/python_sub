ff5aaffe158df92236020dd92045af082676c1f0
shwetabagade26 <shwetambagade26@gmail.com>
2024-03-14 17:51:34 +0530

    practice test

--- practice_test_14_03_24/question6.py
+++ practice_test_14_03_24/question6.py
@@ -1,6 +1,7 @@
 # given a list of strings reprseneting the file paths .each file path consists of directories separated by slashes / 
 # write a function to count the frequency of each directory and return the directory hierarchy as a nested dictionary
 
+
 file_paths = [
     "/home/user/documents/report.txt",
     "/home/user/documents/project1/specs.txt",
@@ -9,30 +10,29 @@ file_paths = [
     "/home/user/pictures/image.jpg"
 ]
 
+def count_directory_frequency(file_paths):
+    directory_frequency = {}
 
-def hierarchy(path):
-    '''
-    Not Done
-    '''
-    file_dict = {}
-    for files in path:
-        files = files.split("/")
-        files.pop(0)
-        # print(files)
-        for file in files:
-                if file.isalnum():
-                    pass
-                else:
-                     frequency = 1
-                    #  print(frequency)
-    print({"home": {"user":{"documents":{"report.txt":frequency,"project1":
-                                         {"specs.txt":frequency,"code":{"main.py":frequency}},
-                                         "project2":{"notes.txt":frequency}}},
-                                         "pictures":{"image.jpg":frequency}}})
+    for file_path in file_paths:
+        directories = file_path.split('/')
+        directories.pop(0)
+        current_directory = directory_frequency
 
-    # print(file_dict)
-    
+        for directory in directories[:-1]:  # Iterate over directories, excluding the file name
+            if directory not in current_directory:
+                current_directory[directory] = {}  # Create a nested dictionary for subdirectories
+            current_directory = current_directory[directory]
 
-hierarchy(file_paths)
+        filename = directories[-1]  # Get the file name
+        if filename not in current_directory:
+            current_directory[filename] = 0  # Initialize frequency count
+        current_directory[filename] += 1  # Increment frequency count
+
+    return directory_frequency
+
+
+
+result = count_directory_frequency(file_paths)
+print(result)
 
 
