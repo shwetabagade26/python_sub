1a4ba156b348eedb6efe498a897a733c9cf96235
shwetabagade26 <shwetambagade26@gmail.com>
2024-03-14 15:14:45 +0530

    practice test

--- practice_test_14_03_24/question2.py
+++ practice_test_14_03_24/question2.py
@@ -1,1 +1,16 @@
 # create pythagorean triplets with sides less than or equal to 30
+
+def pythatrip(terms):
+    ls = []
+    for i in range(1,terms+1):
+        for j in range(2,terms+1):
+            for k in range(3,terms+1):
+                if k**2 == j**2 + i**2:
+                    ls.append((i,j,k))
+                    break
+    print(ls)
+                       
+
+pythatrip(15)
+
+
--- practice_test_14_03_24/question5.py
+++ practice_test_14_03_24/question5.py
@@ -1,1 +1,4 @@
 # round values for each item present in the list using map function
+Input_list = [2.6743, 3.63526, 4.2325, 5.9687967, 6.3265, 7.6988, 8.232, 9.6907 ]
+print("Using list comprehension   ",[round(i) for i in Input_list])
+print("Using Map Function         ",list(map(round,Input_list)))
